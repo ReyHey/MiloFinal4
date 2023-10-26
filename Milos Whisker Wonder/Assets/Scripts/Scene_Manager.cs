af3be379fc5cb05105549790bd03571be731d0e6
ReyHey <58726337+ReyHey@users.noreply.github.com>
2023-10-25 21:23:15 -0700

    Scene Changer Works
    It works, but it is hard coded right now

--- Milos Whisker Wonder/Assets/Scripts/Scene_Manager.cs
+++ Milos Whisker Wonder/Assets/Scripts/Scene_Manager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Exit_Game : MonoBehaviour
 {
@@ -10,15 +11,18 @@ public class Exit_Game : MonoBehaviour
         
     }
 
-    void QuitGame()
+    public void QuitGame()
     {
         UnityEngine.Application.Quit();
         Debug.Log("Quitting");
 
     }
-
-    // Update is called once per frame
-    void Update()
+	public void ChangeScene()
+	{
+		SceneManager.LoadScene("SampleScene");
+	}
+	// Update is called once per frame
+	void Update()
     {
         
     }
