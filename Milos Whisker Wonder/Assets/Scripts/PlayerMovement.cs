e85ea697717b53e38fd2ab222a26bd5d7a936cb7
beestrubz <brandonstruble@ymail.com>
2023-11-27 18:55:30 -0800

    uphill bug
    Currently trying to fix how Milo handles slopes. He thinks he is jumping when going up a hill and think he is falling when going down a hill.

--- Milos Whisker Wonder/Assets/Scripts/PlayerMovement.cs
+++ Milos Whisker Wonder/Assets/Scripts/PlayerMovement.cs
@@ -4,14 +4,30 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] float jumpForce = 5f;
-    [SerializeField] float runSpeed = 7f;
+    
     private SpriteRenderer sprite;
     private float dirX = 0f;
+
+    // Component References
     private Rigidbody2D rb;
-    private BoxCollider2D coll;
+    //private BoxCollider2D coll;
+    private CapsuleCollider2D coll;
+
+    private Vector2 colliderSize;
+    private float slopeDownAngle;
+    private Vector2 slopNormalPerp;
+    private bool isOnSlope;
+    private float slopeDownAngleOld;
+    private bool isGrounded;
+    private Vector2 newVelocity;
+    private float slopeSideAngle;
+
     private Animator anim;
     [SerializeField] private LayerMask jumpableGround;
+    [SerializeField] float jumpForce = 5f;
+    [SerializeField] float runSpeed = 7f;
+    [SerializeField] float slopeCheckDistance;
+   
 
     private enum MovementState{running, jumping, falling, idle}
 
@@ -19,27 +35,106 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     private void Start()
     {
-        /// Bye Bye
         rb = GetComponent<Rigidbody2D>();
         sprite = GetComponent<SpriteRenderer>();
-        coll = GetComponent<BoxCollider2D>();
+        //coll = GetComponent<BoxCollider2D>();
+        coll = GetComponent<CapsuleCollider2D>();
+        colliderSize = coll.size;
         anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     private void Update()
     {
+
+        Jump();
+        UpdateAnimationUpdate();
+        SlopeCheck();
+        
+        if (isGrounded && !isOnSlope)
+        {
+            newVelocity.Set(runSpeed * dirX, 0.0f);
+            rb.velocity = newVelocity;
+        }
+
+        else if (isGrounded && isOnSlope)
+        {
+            newVelocity.Set(runSpeed * slopNormalPerp.x * -dirX, runSpeed * slopNormalPerp.y * -dirX);
+            rb.velocity = newVelocity;
+        }
+
+        
+        else if (!isGrounded)
+        {
+            newVelocity.Set(runSpeed * dirX, rb.velocity.y);
+            rb.velocity = newVelocity;
+        }
+
+    }
+
+    private void SlopeCheck()
+    {
+        Vector2 checkPos = transform.position - new Vector3(0.0f, colliderSize.y / 2);
+
+        SlopCheckVertical(checkPos);
+        SlopCheckHorizontal(checkPos);
+
+    }
+
+    private void SlopCheckHorizontal(Vector2 checkPos)
+    {
+        RaycastHit2D slopeHitFront = Physics2D.Raycast(checkPos, transform.right, slopeCheckDistance, jumpableGround);
+        RaycastHit2D slopeHitBack = Physics2D.Raycast(checkPos, -transform.right, slopeCheckDistance, jumpableGround);
+
+        if (slopeHitFront)
+        {
+            isOnSlope = true;
+            slopeSideAngle = Vector2.Angle(slopeHitFront.normal, Vector2.up);
+        }
+        else if (slopeHitBack)
+        {
+            isOnSlope = true;
+            slopeSideAngle = Vector2.Angle(slopeHitBack.normal, Vector2.up);
+        }
+
+        else
+        {
+            slopeSideAngle = 0.0f;
+            isOnSlope = false;
+        }
+    }
+
+    private void SlopCheckVertical(Vector2 checkPos)
+    {
+        RaycastHit2D hit = Physics2D.Raycast(checkPos, Vector2.down, slopeCheckDistance, jumpableGround);
+
+        if (hit)
+        {
+            slopNormalPerp = Vector2.Perpendicular(hit.normal).normalized;
+            slopeDownAngle = Vector2.Angle(hit.normal, Vector2.up);
+
+            if (slopeDownAngle != slopeDownAngleOld)
+            {
+                isOnSlope = true;
+            }
+
+            slopeDownAngleOld = slopeDownAngle;
+
+            Debug.DrawRay(hit.point, slopNormalPerp, Color.red);
+            Debug.DrawRay(hit.point, hit.normal, Color.green);
+        }
+    }
+
+    private void Jump()
+    {
         dirX = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(dirX * runSpeed, rb.velocity.y);
-        if(Input.GetButtonDown("Jump") && IsGrounded())
+        if (Input.GetButtonDown("Jump") && IsGrounded())
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
         }
-
-        UpdateAnimationUpdate();
-          
     }
- 
+
     private void UpdateAnimationUpdate()
     {
         MovementState state;
@@ -68,7 +163,7 @@ public class PlayerMovement : MonoBehaviour
         {
             state = MovementState.jumping;
         }
-        else if(rb.velocity.y < -0.1f)
+        else if(rb.velocity.y < -0.1f && IsGrounded())
         {
             state = MovementState.falling;
         }
@@ -76,7 +171,12 @@ public class PlayerMovement : MonoBehaviour
         anim.SetInteger("movementState", (int)state);
     }
 
-    private bool IsGrounded() //checks to see if the player is standing on the ground
+
+  
+
+
+    //checks to see if the player is standing on the ground
+    private bool IsGrounded() 
     {
         return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpableGround); 
     }
